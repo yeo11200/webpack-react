784cb3bdb6b0daca44cb5c6f2d14dd40c54afe3f
신진섭 <yeo11200@hanmail.net>
2022-11-04 00:32:47 +0900

    [master] 공통 Axios 추가 및 module.scss 사용 허용 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './page/Main';
 
 const App = () => {
-    return (
-        <BrowserRouter>
-            <Routes>
-                <Route path='/' element={<Main />} />
-            </Routes>
-        </BrowserRouter> 
-    );
-}
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Main />} />
+			</Routes>
+		</BrowserRouter>
+	);
+};
 export default App;
